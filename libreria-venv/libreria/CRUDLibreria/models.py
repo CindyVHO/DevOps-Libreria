dcf2b67ca112428045d3094098a43a3235fded96
Cindy Hernández <cvho31@gmail.com>
2018-08-23 16:56:20 -0500

    [Libreria] Configuracion administracion de Django

--- libreria-venv/libreria/CRUDLibreria/models.py
+++ libreria-venv/libreria/CRUDLibreria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    nombre = models.CharField('nombre', max_length=100)
+    autor = models.CharField('autor', max_length=100)
+    isbn = models.CharField('isbn', max_length=100)
+    urlImagen = models.CharField('urlImg', max_length=100)
+    editorial = models.CharField('editorial', max_length=100)
+    anioPublicacion = models.CharField('anioPublicacion', max_length=100)
+
+    class Meta:
+        verbose_name = 'libro'
+        verbose_name_plural = 'libros'
+        ordering = ['nombre']
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.nombre, self.autor, self.isbn)
--- libreria-venv/libreria/libreria/settings.py
+++ libreria-venv/libreria/libreria/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CRUDLibreria',
+    'bootstrap3',
+    'bootstrap-themes',
 ]
 
 MIDDLEWARE = [
