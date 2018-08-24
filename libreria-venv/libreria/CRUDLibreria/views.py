02372904f7ad4fed9bc2c15e45bef07966fabd16
Cindy Hernández <cvho31@gmail.com>
2018-08-24 06:40:48 -0500

    [Libreria] Funcionamiento listar libros

--- libreria-venv/libreria/CRUDLibreria/views.py
+++ libreria-venv/libreria/CRUDLibreria/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from rest_framework import serializers
 from .models import Libro
+
 # Create your views here.
 def listarLibros(request):
     lista_libros = Libro.objects.all()
     context = {'lista_libros': lista_libros}
-    return render(request, 'index.html', context)
+    return HttpResponse(se)
 
--- libreria-venv/libreria/libreria/settings.py
+++ libreria-venv/libreria/libreria/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'CRUDLibreria',
     'bootstrap3',
     'bootstrap_themes',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.DjangoModelPermissions',),
+    'PAGINATE_BY': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
