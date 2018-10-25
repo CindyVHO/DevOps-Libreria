834d576916bbdfc36adcbe700258ff17ece300bd
Cindy Hernández <cvho31@gmail.com>
2018-10-25 05:41:43 -0500

    [Libreria] CRUD

--- libreria-venv/libreria/CRUDLibreria/urls.py
+++ libreria-venv/libreria/CRUDLibreria/urls.py
@@ -4,5 +4,9 @@ from . import  views
 
 urlpatterns = [
     url(r'^$', views.listarLibros, name='listaLibros'),
+    url(r'^add/$', views.agregarLibros, name='agregarLibros'),
+    url(r'^(?P<libroid>\d+)/', views.actualizarLibros, name='actualizarLibros'),
+    url(r'^(?P<libroid>\d+)/delete/', views.eliminarLibros, name='eliminarLibros'),
+
 ]
 
--- libreria-venv/libreria/CRUDLibreria/views.py
+++ libreria-venv/libreria/CRUDLibreria/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render, HttpResponse
-from rest_framework import serializers
+from django.shortcuts import render, render_to_response, HttpResponseRedirect, get_object_or_404
+from django.contrib import messages
 from .models import Libro
+from .forms import LibroForm
 
 # Create your views here.
 def listarLibros(request):
     lista_libros = Libro.objects.all()
     context = {'lista_libros': lista_libros}
-    return HttpResponse(se)
+    return render(request, 'listar.html', context)
 
+def agregarLibros(request):
+    form = LibroForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "The post has been saved!")
+            return HttpResponseRedirect("/CRUDLibreria/")
+
+    return render(request, 'agregarForm.html', {'form': form})
+
+
+def actualizarLibros(request, libroid):
+    instance = get_object_or_404(Libro, id=libroid)
+    form = LibroForm(request.POST or None, instance=instance)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "El libro ha sido actualizado")
+            return HttpResponseRedirect("/CRUDLibreria/")
+
+    return render(request, 'agregarForm.html', {'form': form})
+
+
+def eliminarLibros(request, libroid):
+    instance = get_object_or_404(Libro, id=libroid)
+    instance.delete()
+    messages.add_message(request, messages.SUCCESS, "El libro con id %s ha sido eliminado!" % libroid)
+    return HttpResponseRedirect("/CRUDLibreria/")
