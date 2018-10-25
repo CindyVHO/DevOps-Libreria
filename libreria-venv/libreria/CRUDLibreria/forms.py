aedbd8d01c62160befce6b7c9159716914e6d14b
Cindy Hernández <cvho31@gmail.com>
2018-10-25 07:06:59 -0500

    [Libreria] Refactor forms.py

--- libreria-venv/libreria/CRUDLibreria/forms.py
+++ libreria-venv/libreria/CRUDLibreria/forms.py
@@ -1,15 +1,7 @@
 from django import forms
 from .models import Libro
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
         fields = ('nombre', 'autor', 'isbn', 'urlImagen', 'editorial', 'anioPublicacion')
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.helper = FormHelper()
-            self.helper.form_method = 'post'
-            self.helper.add_input(Submit('submit', 'Guardar Libro'))
